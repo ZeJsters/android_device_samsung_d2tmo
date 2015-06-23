6bf0a6deb0a84fbf8683e1f635830109a9e1a5a3
Zoher Jetpurwala <zoher.jetpurwala@gmail.com>
2015-06-23 11:49:23 +0530

    D2tmo Bringup for MerkMod

---
+++ merk.mk
@@ -0,0 +1,17 @@
+$(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/merk/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/merk/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2tmo \
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUEMJC:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUEMJC release-keys"
+
+PRODUCT_NAME := merk_d2tmo
+PRODUCT_DEVICE := d2tmo
+
